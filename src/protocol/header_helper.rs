cf2bab0e389e124257ab550970850e7665fa0935
tickbh <tickdream125@hotmail.com>
2023-10-09 11:08:07 +0800

    client

--- src/protocol/header_helper.rs
+++ src/protocol/header_helper.rs
@@ -19,12 +19,11 @@ impl HeaderHelper {
                 continue;
             }
 
-            Self::rewrite_header(request.headers_mut(), &h[1..]);
-            
+            Self::rewrite_header(Some(request), None, &h[1..]);
         }
     }
 
-    pub fn rewrite_response<T>(request: &mut Response<T>, headers: &Vec<Vec<String>>)
+    pub fn rewrite_response<T>(response: &mut Response<T>, headers: &Vec<Vec<String>>)
     where
         T: Serialize,
     {
@@ -36,12 +35,12 @@ impl HeaderHelper {
                 continue;
             }
 
-            Self::rewrite_header(request.headers_mut(), &h[0..]);
+            Self::rewrite_header(None, Some(response), &h[0..]);
             
         }
     }
 
-    pub fn rewrite_header(header: &mut HeaderMap, value: &[String]) {
+    pub fn rewrite_header<T: Serialize>(mut request: Option<&mut Request<T>>, mut response: Option<&mut Response<T>>, value: &[String]) {
         if value.len() < 2 {
             return;
         }
@@ -50,45 +49,90 @@ impl HeaderHelper {
                 if value.len() < 3 {
                     return;
                 }
-                let v = Self::convert_value(header, value[2].to_string());
-                header.push(value[1].to_string(), v);
+                let v = Self::convert_value(&mut request, &mut response, value[2].to_string());
+                if request.is_some() {
+                    request.unwrap().headers_mut().push(value[1].to_string(), v);
+                } else {
+                    response.unwrap().headers_mut().push(value[1].to_string(), v);
+                }
             }
             Self::OP_DEL => {
                 if value.len() < 2 {
                     return;
+                }if request.is_some() {
+                    request.unwrap().headers_mut().remove(&value[1]);
+                } else {
+                    response.unwrap().headers_mut().remove(&value[1]);
                 }
-                header.remove(&value[1]);
             }
             Self::OP_DEFAULT => {
                 if value.len() < 3 {
                     return;
                 }
-                if header.contains(&value[1]) {
+                let contains = if request.is_some() {
+                    request.as_ref().unwrap().headers().contains(&value[1])
+                } else {
+                    response.as_ref().unwrap().headers().contains(&value[1])
+                };
+
+                if contains {
                     return;
                 }
-                let v = Self::convert_value(header, value[2].to_string());
-                header.insert(value[1].to_string(), v);
+                let v = Self::convert_value(&mut request, &mut response, value[2].to_string());
+                if request.is_some() {
+                    request.unwrap().headers_mut().push(value[1].to_string(), v);
+                } else {
+                    response.unwrap().headers_mut().push(value[1].to_string(), v);
+                }
             }
             _ => {
                 if value.len() < 2 {
                     return;
                 }
-                let v = Self::convert_value(header, value[1].to_string());
-                header.insert(value[0].to_string(), v);
+                let v = Self::convert_value(&mut request, &mut response, value[1].to_string());
+                if request.is_some() {
+                    request.unwrap().headers_mut().push(value[0].to_string(), v);
+                } else {
+                    response.unwrap().headers_mut().push(value[0].to_string(), v);
+                }
             }
         }
     }
 
-    fn convert_value(header: &mut HeaderMap, value: String) -> String {
+    fn convert_value<T: Serialize>(request: &mut Option<&mut Request<T>>, response: &mut Option<&mut Response<T>>, value: String) -> String {
         if value.len() == 0 {
             return value;
         }
         if value.as_bytes()[0] == b'$' {
-            if let Some(convert) = header.system_get(&value) {
-                println!("get {} convert {}", value, convert);
-                return convert.to_string();
+            if request.is_some() {
+                if let Some(convert) = request.as_mut().unwrap().headers_mut().system_get(&value) {
+                    println!("get {} convert {}", value, convert);
+                    return convert.to_string();
+                } else {
+                    match &*value {
+                        "$host" => {
+                            return request.as_ref().unwrap().get_host().unwrap_or(String::new());
+                        }
+                        "$url" => {
+                            println!("get {} convert {}", value, format!("{}", request.as_ref().unwrap().url()));
+                            return format!("{}", request.as_ref().unwrap().url());
+                        }
+                        _ => {
+                            return "unknown".to_string();
+                        }
+                    }
+                }
             } else {
-                return "unknown".to_string();
+                if let Some(convert) = response.as_mut().unwrap().headers_mut().system_get(&value) {
+                    println!("get {} convert {}", value, convert);
+                    return convert.to_string();
+                } else {
+                    match &*value {
+                        _ => {
+                            return "unknown".to_string();
+                        }
+                    }
+                }
             }
         }
         return value;
