61904f6238a125f5edc7a287b0ab3aa6557f36d9
tickbh <tickdream125@hotmail.com>
2023-11-30 11:14:48 +0800

    add lincese

--- examples/client.rs
+++ examples/client.rs
@@ -1,7 +1,16 @@
-
+// Copyright 2022 - 2023 Wenmeng See the COPYRIGHT
+// file at the top-level directory of this distribution.
+// 
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+// 
+// Author: tickbh
+// -----
+// Created Date: 2023/09/12 02:36:35
 
 use std::time::{Instant, Duration};
-
 use webparse::{Request, HeaderName};
 use wenmeng::{Client, ProtResult};
 
--- src/protocol/header_helper.rs
+++ src/protocol/header_helper.rs
@@ -1,3 +1,15 @@
+// Copyright 2022 - 2023 Wenmeng See the COPYRIGHT
+// file at the top-level directory of this distribution.
+// 
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+// 
+// Author: tickbh
+// -----
+// Created Date: 2023/10/09 08:30:28
+
 use webparse::{Serialize, Request, Response, HeaderName, HeaderMap, Version};
 
 use crate::{RecvStream, ProtResult, Consts, RecvResponse, RecvRequest};
